0285c2c92b9bf17c9c1d4c955645c489380a6064
trung0501 <nguyentrungt151@gmail.com>
2025-06-18 16:00:30 +0700

    vigenère

---
+++ Vigenère.py
@@ -0,0 +1,29 @@
+def tao_khoa(van_ban, khoa):
+    khoa = list(khoa)
+    if len(van_ban) == len(khoa):
+        return khoa
+    else:
+        for i in range(len(van_ban) - len(khoa)):
+            khoa.append(khoa[i % len(khoa)])
+    return "".join(khoa)
+
+def ma_hoa_vigenere(van_ban, khoa):
+    ma_hoa_van_ban = []
+    khoa = tao_khoa(van_ban, khoa)
+    for i in range(len(van_ban)):
+        char = van_ban[i]
+        if char.isupper():
+            ma_hoa_char = chr((ord(char) + ord(khoa[i]) - 2 * ord('A')) % 26 + ord('A'))
+        elif char.islower():
+            ma_hoa_char = chr((ord(char) + ord(khoa[i]) - 2 * ord('a')) % 26 + ord('a'))
+        else:
+            ma_hoa_char = char
+        ma_hoa_van_ban.append(ma_hoa_char)
+    return "".join(ma_hoa_van_ban)
+
+# Example usage
+van_ban_de_ma_hoa = "NGUYENTIENTRUNG"
+khoa = "CHUYENGIA"
+
+ma_hoa_van_ban = ma_hoa_vigenere(van_ban_de_ma_hoa, khoa)
+print(f"Văn bản mã hóa: {ma_hoa_van_ban}")
