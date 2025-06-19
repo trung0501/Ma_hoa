5991f1bb82a0c5a79997e4b5a3f4a3ef021d66f7
trung0501 <nguyentrungt151@gmail.com>
2025-06-19 10:38:27 +0700

    vigenère

--- Vigenère.py
+++ Vigenère.py
@@ -1,17 +1,17 @@
-def tao_khoa(van_ban, khoa):
+def tao_khoa(ban_ro, khoa):
     khoa = list(khoa)
-    if len(van_ban) == len(khoa):
+    if len(ban_ro) == len(khoa):
         return khoa
     else:
-        for i in range(len(van_ban) - len(khoa)):
+        for i in range(len(ban_ro) - len(khoa)):
             khoa.append(khoa[i % len(khoa)])
     return "".join(khoa)
 
-def ma_hoa_vigenere(van_ban, khoa):
+def ma_hoa_vigenere(ban_ro, khoa):
     ma_hoa_van_ban = []
-    khoa = tao_khoa(van_ban, khoa)
-    for i in range(len(van_ban)):
-        char = van_ban[i]
+    khoa = tao_khoa(ban_ro, khoa)
+    for i in range(len(ban_ro)):
+        char = ban_ro[i]
         if char.isupper():
             ma_hoa_char = chr((ord(char) + ord(khoa[i]) - 2 * ord('A')) % 26 + ord('A'))
         elif char.islower():
@@ -22,11 +22,11 @@ def ma_hoa_vigenere(van_ban, khoa):
     return "".join(ma_hoa_van_ban)
 
 # Dữ liệu đầu vào
-ban_ro = "NGUYENTIENTRUNG"
+van_ban_ma_hoa = "NGUYENTIENTRUNG"
 khoa = "CHUYENGIA"
 
 # Gọi hàm để thực hiện mã hóa
-ma_hoa_van_ban = ma_hoa_vigenere(ban_ro, khoa)
+ma_hoa_van_ban = ma_hoa_vigenere(van_ban_ma_hoa, khoa)
 
 # Kết quả 
 print(f"Văn bản mã hóa: {ma_hoa_van_ban}")
